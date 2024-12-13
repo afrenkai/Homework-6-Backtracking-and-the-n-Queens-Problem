8fc1a63f6f5e4e16ab7fcc3d36c1ba8696832aea
afrenk <afrenk@wpi.edu>
2024-12-12 20:19:15 -0500

    final submission

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Homework-6-Backtracking-and-the-n-Queens-Problem
+
+
+Usage: \
+`java NQueens.java`
--- nQueens.java
+++ nQueens.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 
 public class NQueens {
+   
     public static boolean isLegalPosition(int[] board, int placed) {
         int currentRow = placed - 1;
         for (int i = 0; i < currentRow; i++) {
@@ -30,13 +31,38 @@ public class NQueens {
 
     public static int[] findFirstSolution(int n) {
         int[] board = new int[n];
-        return NextLegalPosition(board, n);
+        return NextLegalPosition(board, n); 
+    }
+
+    public static void countSolutions(int[] board, int row, int n, int[] count) {
+        if (row == n) {
+            count[0]++;
+            return;
+        }
+
+        for (int col = 1; col <= n; col++) {
+            board[row] = col;
+            if (isLegalPosition(board, row + 1)) {
+                countSolutions(board, row + 1, n, count);
+            }
+            board[row] = 0;
+        }
+    }
+
+    public static int calculateTotalSolutions(int n) {
+        int[] board = new int[n];
+        int[] count = {0};
+        countSolutions(board, 0, n, count);
+        return count[0];
     }
 
     public static void main(String[] args) {
-        for (int n = 4; n <= 100; n++) {
-            int[] solution = findFirstSolution(n);
-            System.out.println(n + "-Queens first solution: " + Arrays.toString(solution));
+        for (int n = 4; n <= 14; n++) {
+            int[] firstSolution = findFirstSolution(n);
+            int totalSolutions = calculateTotalSolutions(n);
+
+            System.out.println(n + "-Queens first solution: " + Arrays.toString(firstSolution));
+            System.out.println("There are " + totalSolutions + " solutions to the " + n + "-Queens Problem.");
         }
     }
 }
